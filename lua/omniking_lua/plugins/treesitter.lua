e71eec7e75d5b08e1ac2db9b3a5b777bdf03d2b3
OmniKingDev <omniking.developer.eveos@gmail.com>
2026-09-20 11:18:11 -0700

    Added 'nvim-autopairs' plugin: TS updated

--- lua/omniking_lua/plugins/treesitter.lua
+++ lua/omniking_lua/plugins/treesitter.lua
@@ -3,5 +3,11 @@ return {
         "nvim-treesitter/nvim-treesitter",
         lazy = false,
         build = ":TSUpdate",
+        opts = function(_, opts)
+            -- Ensure both c and cpp parsers are installed
+            if type(opts.ensure_installed) == "table" then
+                vim.list_extend(opts.ensure_installed, { "c", "cpp" })
+            end
+        end,
     },
 }
